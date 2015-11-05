a8a4a4f8192f00a1bcf40eaa5da1112e027e7289
Rosen Stoyanov <me@pockata.org>
2015-11-05 16:15:39 +0200

    [RandR] Changes work layout

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1680x600 --rotate normal --output VGA1 --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 256x1080 --rotate normal --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
