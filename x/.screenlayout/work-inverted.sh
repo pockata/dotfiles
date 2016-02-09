fe689fbb9b408dc9e7a6e19140911d7fb94068e8
Rosen Stoyanov <me@pockata.org>
2016-02-09 15:53:32 +0200

    [X] Updates layout files

--- x/.screenlayout/work-inverted.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x640 --rotate normal --output VGA1 --mode 1680x1050 --pos 1366x0 --rotate normal
