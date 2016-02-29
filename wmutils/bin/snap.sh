505d0af928c31135600bc0c755e384531f246b62
Rosen Stoyanov <me@pockata.org>
2016-02-29 11:16:16 +0200

    [wmutils] Adds multimon snap capabilities

--- wmutils/bin/snap.sh
+++ wmutils/bin/snap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# wmutils/contrib>: snap.sh, 9/12/15 kekler 
+# wmutils/contrib>: snap.sh, 9/12/15 kekler
 # snaps focued window to the left, right, top, or bottom edge
 
 usage() {
@@ -9,7 +9,7 @@ usage() {
 }
 
 # default values for gaps and master area
-TOP_PANEL=${PANEL:-20}
+TOP_PANEL=${PANEL:-30}
 GAP=${GAP:-5}
 
 # get current window id and its borderwidth
@@ -17,24 +17,24 @@ PFW=$(pfw)
 BW=$(wattr b $PFW)
 
 # get root window's size
-ROOT=$(lsw -r)
-SW=$(wattr w $ROOT)
-SH=$(wattr h $ROOT)
+mon=($(mattr wh $PFW))
+SW="${mon[0]}"
+SH="${mon[1]}"
 
 # calculate usable screen size (without borders and gaps)
 SH=$((SH - TOP_PANEL))
 
-snap_up() 
+snap_up()
 {
     wtp $GAP $((GAP + TOP_PANEL)) $((SW - 2*GAP - 2*BW)) $((SH/2 - 2*BW - GAP - GAP/2)) $PFW
 }
 
-snap_right() 
+snap_right()
 {
     wtp $((SW - SW/2 + GAP/2)) $((GAP + TOP_PANEL)) $((SW/2 - 2*BW - GAP - GAP/2)) $((SH - 2*BW - 2*GAP)) $PFW
 }
 
-snap_down() 
+snap_down()
 {
     wtp $GAP $((SH - SH/2 + GAP/2 + TOP_PANEL)) $((SW - 2*GAP - 2*BW)) $((SH/2 - 2*BW - GAP - GAP/2)) $PFW
 }
