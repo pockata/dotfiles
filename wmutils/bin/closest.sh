322c4df3e863366e26822d45b0a962abcee5e751
Rosen Stoyanov <me@pockata.org>
2016-03-26 21:21:58 +0000

    [wmutils] New closest.sh script

--- wmutils/bin/closest.sh
+++ wmutils/bin/closest.sh
@@ -1,37 +1,24 @@
 #!/bin/sh
 #
-# z3bra - 2014 (c) wtfpl
-# find and focus the closest window in a specific direction
-# depends on: focus.sh
-
-# get current window id
-CUR=$(pfw)
+# closest - focus the closest window
+# (c) ix & milsen 2016
+#
 
 usage() {
-    echo "usage: $(basename $0) <direction>" >&2
-    exit 1
+  echo "usage: $(basename $0) <direction>" >&2
+  exit 1
 }
 
-next_east() {
-    lsw | xargs wattr xi | sort -nr | sed "0,/$CUR/d" | sed "1s/^[0-9]* //p;d"
+CUR=$(pfw)
+
+next() {
+  lsw | xargs wattr "$1"i | sort -n"$2" | sed "0,/$CUR/d" | sed "1s/^[0-9]* //p;d"
 }
 
-next_west() {
-    lsw | xargs wattr xi | sort -n | sed "0,/$CUR/d" | sed "1s/^[0-9]* //p;d"
-}
-
-next_north() {
-    lsw | xargs wattr yi | sort -nr | sed "0,/$CUR/d" | sed "1s/^[0-9]* //p;d"
-}
-
-next_south() {
-    lsw | xargs wattr yi | sort -n | sed "0,/$CUR/d" | sed "1s/^[0-9]* //p;d"
-}
-
-# Use the specification of your choice: WASD, HJKL, ←↑↓→, west/north/south/east
 case $1 in
-    h|a|east|left)  focus.sh $(next_east)  2>/dev/null ;;
-    j|s|south|down) focus.sh $(next_south) 2>/dev/null ;;
-    k|w|north|up)   focus.sh $(next_north) 2>/dev/null ;;
-    l|d|west|right) focus.sh $(next_west)  2>/dev/null ;;
+  h|a|east|left)  focus.sh $(next x r)  2>/dev/null ;;
+  j|s|south|down) focus.sh $(next y "") 2>/dev/null ;;
+  k|w|north|up)   focus.sh $(next y r)  2>/dev/null ;;
+  l|d|west|right) focus.sh $(next x "") 2>/dev/null ;;
+  *) usage ;;
 esac
