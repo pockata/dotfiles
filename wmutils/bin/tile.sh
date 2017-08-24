4c327b3e7fbfd5c1ce4558e2d12bd3c7600384eb
Rosen Stoyanov <me@pockata.org>
2017-08-24 23:14:53 +0300

    [wmutils] Multimonitor tile.sh

--- wmutils/bin/tile.sh
+++ wmutils/bin/tile.sh
@@ -1,39 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 #
 # z3bra - 2014 (c) wtfpl
 # arrange windows in a tiled pattern
-
-# default values for gaps and master area
-PANEL=${PANEL:-0}
-GAP=${GAP:-20}
-MASTER=${MASTER:-900}
 
 # get current window id and its borderwidth
 PFW=$(pfw)
 BW=$(wattr b $PFW)
 
-# get root window's size (beware, multi-head setups...)
-ROOT=$(lsw -r)
-SW=$(wattr w $ROOT)
-SH=$(wattr h $ROOT)
+# default values for gaps and master area
+PANEL=${PANEL:-45}
+GAP=${GAP:-15}
+# occupy 60% of the screen width
+MASTER=${MASTER:-$(echo "$(mattr w $PFW) * 60 / 100" | bc)}
 
-# get the number of windows to put in the stacking area
-MAX=$(lsw|grep -v $PFW|wc -l)
+# get current monitor's size
+SW=$(mattr w $PFW)
+SH=$(mattr h $PFW)
+SX=$(mattr x $PFW)
+SY=$(mattr y $PFW)
+
+# get the windows in the current monitor
+# TODO: simplify this. lsw -m <monitor id>?
+WINDOWS=$(paste <(lsw | xargs mattr i) <(lsw) | grep -i $(mattr i $(pfw)) | awk '{ print $2; }' | grep -v $PFW)
+MAX=$(echo "$WINDOWS" | wc -l)
 
 # calculate usable screen size (without borders and gaps)
 SW=$((SW - GAP - 2*BW))
-SH=$((SH - GAP - 2*BW - PANEL))
+SH=$((SH - GAP - PANEL))
 
-Y=$((0 + GAP + PANEL))
+Y=$((SY + GAP + PANEL))
 # put current window in master area
-wtp $GAP $Y $((MASTER - GAP - 2*BW)) $((SH - GAP)) $PFW
+wtp $((SX + GAP)) $Y $((MASTER - GAP - 2*BW)) $((SH - GAP - 2*BW)) $PFW
 
 # and now, stack up all remaining windows on the right
-X=$((MASTER + GAP))
+X=$((MASTER + GAP + SX))
 W=$((SW - MASTER - GAP))
 H=$((SH / MAX - GAP - 2*BW))
 
-for wid in $(lsw|grep -v $PFW); do
+for wid in $WINDOWS; do
     wtp $X $Y $W $H $wid
     Y=$((Y + H + GAP + 2*BW))
 done
+
