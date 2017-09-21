ee013920952ceae280d3765a0d1c16424c1e17f4
Rosen Stoyanov <me@pockata.org>
2017-09-21 11:31:53 +0300

    [utils] Improve screenshot script

--- utils/bin/screenshot.sh
+++ utils/bin/screenshot.sh
@@ -2,7 +2,7 @@
 
 ES="Entire screen"
 SA="Select Area"
-CMD=`echo -e "$ES\n$SA" | rofi -dmenu -i -p "Capture Screen:"`
+CMD=`echo -e "$SA\n$ES" | rofi -dmenu -i -p "Capture Screen:"`
 if [ ! "$CMD" ]; then
     exit
 fi
@@ -11,12 +11,14 @@ FILE="/tmp/screenshot-$(date "+%F-%H_%M_%S").png"
 
 case $CMD in
     $ES)
-        sleep 0.3 && maim --hidecursor -d 1.0 -b 3 -c 250,250,250 $FILE 2>&1 ;;
+        sleep 0.3 && maim --hidecursor -d 1.0 -b 3 -c 0.3,0.4,0.6,0.4 --highlight $FILE 2>&1 ;;
     $SA)
-        sleep 0.3 && maim --hidecursor -d 1.0 -s -b 3 -c 250,250,250 $FILE 2>&1 ;;
+        sleep 0.3 && maim --hidecursor -d 1.0 -s -b 3 -c 0.3,0.4,0.6,0.4 --highlight $FILE 2>&1 ;;
 esac
 
-~/bin/pop "Screenshot taken"
+if [ $? -eq 0 ]; then
+    ~/bin/pop "Screenshot taken"
+fi
 
 ~/bin/imgur-screenshot.sh $FILE
 
