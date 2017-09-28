9a3a2a540414b225fc2f4b71d1be53ac5ee3ef29
Rosen Stoyanov <me@pockata.org>
2017-09-28 21:11:28 +0300

    [utils] Switch to us layout when locking screen

--- utils/bin/lock.sh
+++ utils/bin/lock.sh
@@ -30,5 +30,8 @@ fi
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 # i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
 
+# always swtitch to us layout
+xkb-switch -s us
+
 i3lock --nofork --beep --ignore-empty-password --image /tmp/screen.png
 
