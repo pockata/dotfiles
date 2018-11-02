0469fdead20ad30742bc6848a01b75126cb972fc
Rosen Stoyanov <me@pockata.org>
2018-11-02 20:57:31 +0200

    [utils] Improve Slic3r PE update script

--- utils/bin/update-slic3r.sh
+++ utils/bin/update-slic3r.sh
@@ -5,16 +5,41 @@ set -e
 file="/tmp/batko.html"
 appimage="/tmp/slic3r.AppImage"
 
+install_slicer() {
+    appimage="$1"
+    link="$2"
+
+    curl -L -o "$appimage" "$link"
+
+    sudo mv "$appimage" /opt/slic3r.AppImage
+    sudo chmod u+x /opt/slic3r.AppImage
+
+    cd /tmp/
+    rm -rf "$file"
+    rm -rf "$appimage"
+
+}
+
+echo ""
+echo "Fetching latest release version..."
+echo ""
+
 curl -L -o "$file" https://github.com/prusa3d/Slic3r/releases
 
-link="https://github.com/$(cat "$file" | pup '.release:first-child .release-body a attr{href}' | grep -i 'appimage' | head -1)"
+link="https://github.com/$(cat "$file" | pup '.release:first-child .Details-element a attr{href}' | grep -i 'appimage' | head -1)"
 
-curl -L -o "$appimage" "$link"
+echo ""
+echo "Link for download: "
+echo "$link"
+echo ""
 
-sudo mv "$appimage" /opt/slic3r.AppImage
-sudo chmod u+x /opt/slic3r.AppImage
-
-cd /tmp/
-rm -rf "$file"
-rm -rf "$appimage"
+while true; do
+    read -p "Do you wish to install this release? " yn
+    echo ""
+    case $yn in
+        [Yy]* ) install_slicer "$appimage" "$link"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
