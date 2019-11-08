9abaab00e319b4e2e789e7dc44d56e28aeff5c46
Rosen Stoyanov <me@pockata.org>
2019-11-08 22:10:36 +0200

    [wmutils] Update `yawee` to match new `wew` api

--- wmutils/bin/yawee.sh
+++ wmutils/bin/yawee.sh
@@ -1,43 +1,36 @@
 #!/bin/sh
 
-# [ -z "$CUR_WINDOW" ] && CUR_WINDOW="$(pfw)"
-
-while IFS=: read ev wid; do
+while read ev wid args; do
 
     case $1 in
         -d|--debug) printf '%s\n' "$ev $wid $(pfw)" ;;
     esac
 
     case $ev in
-
-        # click on window
-        4)
+        BUTTON_PRESS)
             if ! wattr o $wid; then
                 wattr "$wid" && vroum.sh "$wid" "nowarp" &
             fi
             ;;
 
-        # window creation
-        16)
+        CREATE)
             if ! wattr o $wid; then
                 corner_mh.sh md "$wid"
             fi
             ;;
 
-        # mapping requests (show window)
-        19)
-
+        MAP)
             if ! wattr o $wid; then
                     vroum.sh "$wid"
             fi
             ;;
 
-        17)
+        DESTROY)
             fullscreen_mh.sh "$wid" "clear"
             ;;
 
         # focus prev window when hiding(unmapping) focused window
-        18)
+        UNMAP)
             if ! wattr $(pfw); then
                 vroum.sh prev "nowarp" 2>/dev/null
             fi
