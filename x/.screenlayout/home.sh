91d0961ee5181ccaca5717b0f59f99c32da51900
Rosen Stoyanov <me@pockata.org>
2020-02-16 15:39:20 +0200

    [x] Update Home monitor layout

--- x/.screenlayout/home.sh
+++ x/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output LVDS1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI3 --off --output VGA1 --mode 1920x1080 --pos 0x1080 --rotate normal --output VIRTUAL1 --off
