467650bcc839e5fd4036813ee194d6ae7df9b8a2
Rosen Stoyanov <me@pockata.org>
2020-04-06 20:14:17 +0300

    [utils] Rearrange windows on monitor disconnect

--- utils/bin/monitors.sh
+++ utils/bin/monitors.sh
@@ -32,6 +32,16 @@ esac
 # Always set the wallpaper after xrandr changes
 nitrogen --restore &
 
+# Get the primary monitor (if specified) or use the widest one
+primaryMonitor=$(lsm -p || lsm | xargs mattr iwh | sort -k2 -r | head -1 | awk '{ print $1 }')
+
+# Move all windows that are outside of the new arrangement into the primary
+# monitor
+for WINDOW in $(lsw)
+do
+    mattr i "$WINDOW" > /dev/null 2>&1 || closest_mon.sh "specific" "$WINDOW" "$primaryMonitor"
+done
+
 # Restart the bar
 # TODO: Find a way to improve it
 kill -9 `pgrep lemonbar` &> /dev/null
