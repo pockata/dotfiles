1d0211bb070399fb7708499c36223d232cee54c5
Rosen Stoyanov <me@pockata.org>
2021-04-23 16:34:14 +0300

    [nvim] Configure LSP Diagnostics
    Disables `underline`

--- nvim/.config/nvim/lua/config/lspinstall.lua
+++ nvim/.config/nvim/lua/config/lspinstall.lua
@@ -53,6 +53,16 @@ local function setup_servers()
 	end
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics,
+	{
+		underline = false,
+		virtual_text = true,
+		signs = true,
+		update_in_insert = false,
+	}
+)
+
 setup_servers()
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
