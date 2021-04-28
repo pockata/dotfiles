36259dad19fbaa62c823bd40c431f477ed3ec9c0
Rosen Stoyanov <me@pockata.org>
2021-04-28 10:01:18 +0300

    [nvim] Update Hop config

--- nvim/.config/nvim/lua/config/hop.lua
+++ nvim/.config/nvim/lua/config/hop.lua
@@ -1,15 +1,33 @@
-require'hop'.setup({
-	keys = 'asdfghjklwertyuixcvbnm'
-	-- keys = 'tnhesoairudpwf'
-})
+require('hop').setup({})
 
 nmap('s', '<silent>', '<cmd>HopChar2<CR>')
+nmap('S', '<silent>', '<cmd>HopWord<CR>')
 
-nmap('f', '<silent>', '<cmd>HopChar1<CR>')
-xmap('f', '<silent>', '<cmd>HopChar1<CR>')
-omap('f', '<silent>', '<cmd>HopChar1<CR>')
+nnoremap('f', '<silent>', '<cmd>call WrapHop("f")<CR>')
+xnoremap('f', '<silent>', '<cmd>call WrapHop("f")<CR>')
+onoremap('f', '<silent>', '<cmd>call WrapHop("f")<CR>')
 
-nmap('t', '<silent>', '<cmd>HopChar1<CR>')
-xmap('t', '<silent>', '<cmd>HopChar1<CR>')
-omap('t', '<silent>', '<cmd>HopChar1<CR>')
+nnoremap('t', '<silent>', '<cmd>call WrapHop("t")<CR>')
+xnoremap('t', '<silent>', '<cmd>call WrapHop("t")<CR>')
+onoremap('t', '<silent>', '<cmd>call WrapHop("t")<CR>')
+
+-- remove the underline from highlighted characters
+create_augroup('HopConfig', {
+	'VimEnter,ColorScheme * highlight HopNextKey gui=bold'
+})
+
+-- Wrap HopChar1 to use t/f if used with a count
+-- Also implement t properly by making the selection non-inclusive
+vim.cmd([[
+function! WrapHop(key)
+	if (v:count)
+		call feedkeys(v:count . a:key, "n")
+	else
+		HopChar1
+		if (a:key == 't')
+			normal! h
+		endif
+	endif
+endfunction
+]])
 
