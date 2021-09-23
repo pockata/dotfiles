73740362175f6fdcf756986bdb014900182ef741
Rosen Stoyanov <me@pockata.org>
2021-09-23 13:25:56 +0300

    [nvim] Support staging hunk via visual lines

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -20,6 +20,7 @@ require('gitsigns').setup {
 		['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
 		['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+		['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
 		['n <leader>hu'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
 		['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
 		['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
