bf0931b975d8c17c512988e4ba98073e48e7698d
Rosen Stoyanov <me@pockata.org>
2021-10-08 12:50:55 +0300

    [nvim] Update LSP diagnostics signs

--- nvim/.config/nvim/lua/config/lspconfig.lua
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -1,14 +1,14 @@
 -- Diagnostic Signs
-vim.fn.sign_define('LspDiagnosticsSignError',
+vim.fn.sign_define('DiagnosticSignError',
 	{ text = '✗', texthl = 'LspDiagnosticsSignError' })
 
-vim.fn.sign_define('LspDiagnosticsSignWarning',
+vim.fn.sign_define('DiagnosticSignWarning',
 	{ text = '', texthl = 'LspDiagnosticsSignWarning' })
 
-vim.fn.sign_define('LspDiagnosticsSignInformation',
+vim.fn.sign_define('DiagnosticSignInformation',
 	{ text = '', texthl = 'LspDiagnosticsSignInformation' })
 
-vim.fn.sign_define('LspDiagnosticsSignHint',
+vim.fn.sign_define('DiagnosticSignHint',
 	{ text = '', texthl = 'LspDiagnosticsSignHint' })
 
 -- Keymaps handled in ./lspinstall
