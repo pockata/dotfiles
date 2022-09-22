c4eb9d13f1015f148dbdd9fc64c62ca6ad6f3352
Rosen Stoyanov <me@pockata.org>
2022-09-22 16:34:19 +0300

    [nvim] Improve harpoon mappings

--- nvim/.config/nvim/lua/config/harpoon.lua
+++ nvim/.config/nvim/lua/config/harpoon.lua
@@ -1,13 +1,13 @@
 vim.cmd [[
-	nnoremap <silent><leader>ta :lua require("harpoon.mark").add_file(); print("Harpooned!")<CR>
+	autocmd Filetype harpoon nnoremap <silent> <buffer> K :silent! move+<CR>|
+		\ nnoremap <silent> <buffer> L :silent! move--<CR>|
+		\ nnoremap <buffer>J <nop>
+
+	nnoremap <silent><leader>ta :lua require("harpoon.mark").add_file()<CR>
 	nnoremap <silent><leader>tc :lua require("harpoon.ui").toggle_quick_menu()<CR>
-	" nnoremap <silent><leader>tc :lua require("harpoon.cmd-ui").toggle_quick_menu()<CR>
 
 	nnoremap <silent><a-j> :lua require("harpoon.ui").nav_file(1)<CR>
 	nnoremap <silent><a-k> :lua require("harpoon.ui").nav_file(2)<CR>
 	nnoremap <silent><a-l> :lua require("harpoon.ui").nav_file(3)<CR>
 	nnoremap <silent><a-;> :lua require("harpoon.ui").nav_file(4)<CR>
-
-	nnoremap <silent><[m> :lua require("harpoon.ui").nav_prev()<CR>
-	nnoremap <silent><]m> :lua require("harpoon.ui").nav_next()<CR>
 ]]
