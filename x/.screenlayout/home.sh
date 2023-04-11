47768a74924a06a49ec46a9f7a344051d7e62b1d
Rosen Stoyanov <me@pockata.org>
2023-04-11 16:13:10 +0300

    update home.sh layout

--- x/.screenlayout/home.sh
+++ x/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-2 --off --output DP-1 --mode 3840x1600 --pos 1080x160 --rotate normal --output HDMI-3 --off --output DP-2 --off --output HDMI-4 --off
+xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-2 --off --output DP-1 --primary --mode 3840x1600 --pos 1080x160 --rotate normal --output HDMI-3 --off --output DP-2 --off --output HDMI-4 --off
