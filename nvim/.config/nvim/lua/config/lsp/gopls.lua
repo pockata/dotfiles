ddb925f215d5ee3f35dfe3c0ee76a344b287c0c2
Rosen Stoyanov <me@pockata.org>
2024-03-19 18:42:54 +0200

    [nvim] Enable go inlay hints

--- nvim/.config/nvim/lua/config/lsp/gopls.lua
+++ nvim/.config/nvim/lua/config/lsp/gopls.lua
@@ -11,6 +11,17 @@ return function(conf)
 				shadow = true,
 			},
 			staticcheck = true,
+
+			-- https://github.com/golang/tools/blob/master/gopls/doc/inlayHints.md
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
 		},
 	}
 	return conf
