dd965445c8650f22ad8b8474c636276a927e1d55
Rosen Stoyanov <me@pockata.org>
2024-05-31 20:35:21 +0300

    [nvim] Update lazy.nvim bootstrap code

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,13 @@
 -- vim: set path+=./lua:
 local g = vim.g
 
--- -- bootstrap Lazy.nvim
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
--- 	vim.fn.system({
--- 		"git",
--- 		"clone",
--- 		"--filter=blob:none",
--- 		"https://github.com/folke/lazy.nvim.git",
--- 		"--branch=stable", -- latest stable release
--- 		lazypath,
--- 	})
--- end
---
--- vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+end
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
 
 -- Disable some built-in plugins we don't want
 local disabled_built_ins = {
