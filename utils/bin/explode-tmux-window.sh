2961c725824fe2fab24a07113e90f9da167e7d0b
Rosen Stoyanov <rosen.stoyanov1@ocado.com>
2024-10-03 16:42:59 +0300

    [tmux][utils] expand tmux window to new alacritty tmux session

---
+++ utils/bin/explode-tmux-window.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+new_session_name="$(tmux display-message -p '#S')-out"
+cur_win=$(tmux display-message -p  "#S:#{window_name}")
+
+if ! tmux has-session -t "${new_session_name}" 2> /dev/null; then
+	tmux new -d -s "${new_session_name}"
+	tmux move-window -t "${new_session_name}" -s "${cur_win}"
+
+	alacritty -e zsh -c "tmux attach -t '${new_session_name}'" &
+	# is this needed?
+	disown
+else
+	tmux move-window -t "${new_session_name}"
+fi
+
